a9f4c7a63572070c54f986efba8f6e54052c5d5e
HelloImDevion <devionfothergill@gmail.com>
2017-08-31 20:52:11 -0400

    add math

--- Week4Methods/Program.cs
+++ Week4Methods/Program.cs
@@ -8,8 +8,41 @@ namespace Week4Methods
 {
     class Program
     {
+        //main is the only thing that runs 
         static void Main(string[] args)
         {
+            int number1 = 25;
+            int number2 = 50;
+            // passing the method (int, int)
+            int total = Add(number1,number2);
+            // Console.WriteLine(total);
+            // passing second method 
+            //Console.WriteLine(Multiply(number2, number1));
+            Greeting();
+            string name = "Will Robenson";
+            RobotWarning(name);
+        }
+        //method 
+        public static int Add(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+            return sum;
+        }
+        //method
+        public static int Multiply(int firstNumber, int secondNumber)
+        {
+            int product = firstNumber * secondNumber;
+            return product;
+        }
+        //method
+        public static void Greeting()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        //method
+        public static void RobotWarning(string name)
+        {
+            Console.WriteLine("Danger!, "+ name);
         }
     }
 }
